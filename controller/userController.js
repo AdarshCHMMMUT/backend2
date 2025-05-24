575d92677802fd04867eb21535c0e7bf178c14e7
AdarshCHMMMUT <97625989+AdarshCHMMMUT@users.noreply.github.com>
2025-05-24 10:48:50 +0530

    added get wish

--- controller/userController.js
+++ controller/userController.js
@@ -1,7 +1,7 @@
 import categoryModel from "../categorymodel.js";
 import Itemmodel from "../itemmodel.js";
 import userModel from "../usermodel.js";
-
+import wishmodel from "../wishmodel.js";
 export const getUserData = async(req,res) =>
 {
   try{
@@ -48,4 +48,17 @@ export const  getCategory = async(req,res)=>{
     return res.json({success:false, message: 'unable to get data ${error.message}'})
   }
   
+}
+export const getwish = async(req, res) =>
+{
+  try{
+    const {userId} = req.body;
+    const user = await wishmodel.findOne({user:userId}).populate('wishlist');
+    if(!user)return res.json({success:false, message: 'user not found'});
+    return res.json({success:true, wishlist: user.wishlist});
+  }
+  catch(error)
+  {
+    return res.json({success:false, message: 'server error',message: error.message});
+  }
 }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,10 @@
 import express from 'express'
-import { getCategory, getItems, getUserData } from '../controller/userController.js';
+import { getCategory, getItems, getUserData, getwish } from '../controller/userController.js';
 
 const userRouter = express.Router();
 
 userRouter.get('/data',  getUserData)
 userRouter.get('/items',getItems)
 userRouter.get('/category',getCategory)
-
+userRouter.get('/getwishlist',getwish)
 export default userRouter;
