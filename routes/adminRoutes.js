f335274a287d9bdf049c9b5933d5a97533195211
AdarshCHMMMUT <97625989+AdarshCHMMMUT@users.noreply.github.com>
2025-05-27 17:12:17 +0530

    populated items

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,8 +1,5 @@
 import e from "express";
 import { addaddon, addcoupon, addstaff, addvariation, adminLogin, adminLogout, createOrder, getstaffs, getvariations, updateStatus } from "../controller/admincontroller.js";
-// import { admin } from "../src/firebase/firebaseadmin.js";
-// import { get } from "mongoose";
-// import { getAddress } from "../controller/adminController.js";
 
 const adminRouter = e.Router();
 adminRouter.post('/adminlogin',adminLogin);
@@ -11,7 +8,6 @@ adminRouter.post('/addstaff', addstaff);
 adminRouter.post('/addcoupon', addcoupon);
 adminRouter.post('/addaddon', addaddon);
 adminRouter.post('/addvariation', addvariation);
-// adminRouter.post('/pushvariation',pushvariation);
 adminRouter.get('/getstaff', getstaffs);
 adminRouter.get('/getvariation', getvariations);    
 adminRouter.post('/createorder', createOrder);
